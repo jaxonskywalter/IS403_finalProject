44463c343e8a1c2b679ea114b16c7733619a13da
parkergeorge13 <parkergeorge13@gmail.com>
2022-11-02 17:05:33 -0600

    added pages

--- WebPages/urls.py
+++ WebPages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import indexPageView, suppliersPageView
+from .views import indexPageView, suppliersPageView, forcastedSalesPageView, summaryPageView
 
 urlpatterns = [
     path("", indexPageView, name="index"), 
     path("suppliers/", suppliersPageView, name="suppliers"), 
+    path("forcastedSales/", forcastedSalesPageView, name="forcastedSales"), 
+    path("summary/", summaryPageView, name="summary"), 
 ] 
--- WebPages/views.py
+++ WebPages/views.py
@@ -4,3 +4,7 @@ def indexPageView(request) :
     return HttpResponse('Crumbl Home Page')  
 def suppliersPageView(request) :
     return HttpResponse('Suppliers Page') 
+def forcastedSalesPageView(request) :
+    return HttpResponse('Forcasted Sales Page') 
+def summaryPageView(request) :
+    return HttpResponse('Summary Page') 
