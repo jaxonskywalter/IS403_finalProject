b91c720fd8899302032a08469a8f8d286c750508
jaxonskywalter <jaxonskywalter@gmail.com>
2022-12-13 20:26:15 -0700

    Add database

--- crumbl/settings.py
+++ crumbl/settings.py
@@ -16,6 +16,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,14 +84,10 @@ WSGI_APPLICATION = 'crumbl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'crumbl',
-    'USER': 'postgres',
-    'PASSWORD': 'Welcome123',
-    'HOST': 'localhost',
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
